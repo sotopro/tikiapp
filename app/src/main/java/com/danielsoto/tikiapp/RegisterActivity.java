16eb04a8c0ab0a6400e2b4a00e581cb773a8a576
dsoto1-rbi <dsoto1@rbi.com>
2022-06-24 13:08:42 -0500

    [feature ] - add ids to edit text & validate

--- app/src/main/java/com/danielsoto/tikiapp/RegisterActivity.java
+++ app/src/main/java/com/danielsoto/tikiapp/RegisterActivity.java
@@ -4,14 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class RegisterActivity extends AppCompatActivity {
     TextView textLogIn;
     MaterialButton registerButton;
+    TextInputEditText inputFullName, inputNickname, inputPhoneNumber , inputPassword;
+    String textFullName, textNickname, textPhoneNumber, textPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +25,10 @@ public class RegisterActivity extends AppCompatActivity {
 
         textLogIn = findViewById(R.id.textLogIn);
         registerButton = findViewById(R.id.register_button);
+        inputFullName = findViewById(R.id.register_input_fullName);
+        inputNickname = findViewById(R.id.register_input_nickname);
+        inputPhoneNumber = findViewById(R.id.register_input_phoneNumber);
+        inputPassword = findViewById(R.id.register_input_password);
 
         textLogIn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,6 +42,14 @@ public class RegisterActivity extends AppCompatActivity {
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                textFullName = inputFullName.getText().toString();
+                textNickname = inputNickname.getText().toString().trim();
+                textPhoneNumber = inputPhoneNumber.getText().toString().trim();
+                textPassword = inputPassword.getText().toString().trim();
+                Log.e("FULLNAME", textFullName);
+                Log.e("NICKNAME", textNickname);
+                Log.e("PHONENUMBER", textPhoneNumber);
+                Log.e("PASSWORD", textPassword);
                 Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
