707a84e6bb9bac044e986ef6ef60ab39402293e6
dsoto1-rbi <dsoto1@rbi.com>
2022-06-24 13:57:25 -0500

    [feature ] - add erros in inputs

--- app/src/main/java/com/danielsoto/tikiapp/RegisterActivity.java
+++ app/src/main/java/com/danielsoto/tikiapp/RegisterActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -16,6 +17,7 @@ import com.google.android.material.textfield.TextInputLayout;
 public class RegisterActivity extends AppCompatActivity {
     TextView textLogIn;
     MaterialButton registerButton;
+    TextInputLayout inputLayoutFullName, inputLayoutNickname, inputLayoutPhoneNumber , inputLayoutPassword;
     TextInputEditText inputFullName, inputNickname, inputPhoneNumber , inputPassword;
     String textFullName, textNickname, textPhoneNumber, textPassword;
     @Override
@@ -24,11 +26,16 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         textLogIn = findViewById(R.id.textLogIn);
+        inputLayoutFullName = findViewById(R.id.register_inputLayout_fullName);
+        inputLayoutNickname = findViewById(R.id.register_inputLayout_nickname);
+        inputLayoutPhoneNumber = findViewById(R.id.register_inputLayout_phoneNumber);
+        inputLayoutPassword = findViewById(R.id.register_inputLayout_password);
         registerButton = findViewById(R.id.register_button);
         inputFullName = findViewById(R.id.register_input_fullName);
         inputNickname = findViewById(R.id.register_input_nickname);
         inputPhoneNumber = findViewById(R.id.register_input_phoneNumber);
         inputPassword = findViewById(R.id.register_input_password);
+
 
         textLogIn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,15 +53,38 @@ public class RegisterActivity extends AppCompatActivity {
                 textNickname = inputNickname.getText().toString().trim();
                 textPhoneNumber = inputPhoneNumber.getText().toString().trim();
                 textPassword = inputPassword.getText().toString().trim();
-                Log.e("FULLNAME", textFullName);
-                Log.e("NICKNAME", textNickname);
-                Log.e("PHONENUMBER", textPhoneNumber);
-                Log.e("PASSWORD", textPassword);
-                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
+
+                if(TextUtils.isEmpty(textFullName) || TextUtils.isEmpty(textNickname) || TextUtils.isEmpty(textPhoneNumber) ||  TextUtils.isEmpty(textPassword) ){
+                    if(TextUtils.isEmpty(textFullName)) {
+                        inputLayoutFullName.setError("Full Name field can't be empty!");
+                    } else {
+                        inputLayoutFullName.setError(null);
+                    }
+                    if(TextUtils.isEmpty(textNickname)) {
+                        inputLayoutNickname.setError("Nickname field can't be empty!");
+                    }
+                    else {
+                        inputLayoutNickname.setError(null);
+                    }
+                    if(TextUtils.isEmpty(textPhoneNumber)) {
+                        inputLayoutPhoneNumber.setError("Phone Number field can't be empty!");
+                    }
+                    else {
+                        inputLayoutPhoneNumber.setError(null);
+                    }
+                    if(TextUtils.isEmpty(textPassword)) {
+                        inputLayoutPassword.setError("Password field can't be empty!");
+                    }
+                    else {
+                        inputLayoutPassword.setError(null);
+                    }
+                } else {
+                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+//
             }
         });
     }
-
 }
