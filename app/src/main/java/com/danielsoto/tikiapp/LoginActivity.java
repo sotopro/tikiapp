aa66f72f54ccf999d85cfad8ce908c1666edf0ca
dsoto1-rbi <dsoto1@rbi.com>
2022-06-24 11:55:21 -0500

    [feature ] - add event to change view

--- app/src/main/java/com/danielsoto/tikiapp/LoginActivity.java
+++ app/src/main/java/com/danielsoto/tikiapp/LoginActivity.java
@@ -2,13 +2,28 @@ package com.danielsoto.tikiapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    TextView textRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        textRegister = findViewById(R.id.textRegister);
+
+        textRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/danielsoto/tikiapp/RegisterActivity.java
+++ app/src/main/java/com/danielsoto/tikiapp/RegisterActivity.java
@@ -2,13 +2,28 @@ package com.danielsoto.tikiapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    TextView textLogIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        textLogIn = findViewById(R.id.textLogIn);
+
+        textLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
+
 }
